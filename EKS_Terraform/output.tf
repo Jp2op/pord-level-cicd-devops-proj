41f0488a0eb4c82bb5083cc2316da5f1ed6089b9
Jp2op <117359287+Jp2op@users.noreply.github.com>
2025-01-04 22:34:32 +0530

    Update output.tf

--- EKS_Terraform/output.tf
+++ EKS_Terraform/output.tf
@@ -1,15 +1,15 @@
 output "cluster_id" {
-  value = aws_eks_cluster.devopsshack.id
+  value = aws_eks_cluster.jpbloggingapp.id
 }
 
 output "node_group_id" {
-  value = aws_eks_node_group.devopsshack.id
+  value = aws_eks_node_group.jpbloggingapp.id
 }
 
 output "vpc_id" {
-  value = aws_vpc.devopsshack_vpc.id
+  value = aws_vpc.jpbloggingapp_vpc.id
 }
 
 output "subnet_ids" {
-  value = aws_subnet.devopsshack_subnet[*].id
+  value = aws_subnet.jpbloggingapp_subnet[*].id
 }
